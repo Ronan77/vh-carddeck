76a2ce5be65459165f8acee15bfd191a45d1eb0d
Ronan77 <alain.nunez55@gmail.com>
2014-11-21 18:42:44 -0500

    Very much done, plus a dealer version
    Trying to fix up the Ace to be either 11 or 1

--- src/21alt.groovy
+++ src/21alt.groovy
@@ -5,13 +5,12 @@ import Card
 Deck deck = new Deck()
 
 // Defining each player for printing
-println("\n\n\n\n\n\n\n\n\n\n\n\n----------------------------------------\nWelcome to 21! By: Ronan_77 (Alain Nuñez)\n----------------------------------------\n\n\n\n")
+println("\n\n\n\n\n\n\n\n\n\n\n\n----------------------------------------\nWelcome to 21! By: Ronan_77 (Alain Nuñez)\n-- Dealer Edition\n----------------------------------------\n\n\n\n")
 
 Scanner scan = new Scanner (System.in)
 
-//Declare the player names
 def player1 = "Alain"
-def player2 = "Tyrell"
+def player2 = "Dealer"
 
 def hand1 = []
 def hand2 = []
@@ -22,18 +21,18 @@ int player2GamesWon = 0
 int handSize = 2
 int numPlayers = 2
 
-//Declare the amount of games to play
+//Delcare the amount of games there will be
 println("- How many games would you like to play?\n")
 int numGames = scan.nextInt()
 
-//Checks if the amount of games is a positive number, if not set it to (1)
+//Make sure the games are a positive number, if not, set it to (1)
 int games = 1
 if (numGames < 1)
     numGames = 1
 
 for (int i = 0; i < numGames; i++)
 {
-    //Shuffle cards, randomly select cards each match
+    //Shuffle and declare random cards each reset of a match
     deck.reset()
     deck.shuffle()
     hand1 = deck.deal(handSize)
@@ -47,14 +46,31 @@ for (int i = 0; i < numGames; i++)
     int player2Play = 1
     int handNum1 = 1
     int handNum2 = 1
-    //Visually sepearates games
+    //Visually shows the change in a match
     println("\n------------------------ Game " + games + "/" + numGames + " ------------------------\n")
+
 
     while (player1Play == 1 || player2Play == 1)
     {
-        //Shows current scores
+        //Shows Current Scores
+        for (int a = 0; a < 1 + handNum1; a++)
+        {
+            if (hand1[a].rank == 11 && (pointP1 > 21))
+            {
+                hand1[a].rank = 1
+                pointP1 -= 10
+            }
+        }
+        for (int a = 0; a < 1 + handNum2; a++)
+        {
+            if (hand2[a].rank == 11 && (pointP2 > 21))
+            {
+                hand2[a].rank = 1
+                pointP2 -= 10
+            }
+        }
         println("------------------------------------------------\n" + player1 + " has " + pointP1 + " points " + hand1 + "\n" + player2 + " has " + pointP2 + " points " + hand2  + "\n------------------------------------------------" + "\n")
-        //Game logic
+        //Game Logic (Probably a bunch in unnesesary)
         if ((pointP1 < 21) && (pointP2 > 21))
         {
             gameWon = 1
@@ -91,19 +107,19 @@ for (int i = 0; i < numGames; i++)
         {
             gameWon = 0
         }
-        //Point logic if there is not a losing or winning point
+        //Point logic if below 20
         if (pointP1 < 21 && pointP2 < 21)
         {
-            //Asks player1 if they like to hit or stand
+            //Player1's choice to have a new card
             println("- " + player1 + " would you like to get a new card? (1 = Yes, 2 = No)\n")
             player1Play = scan.nextInt()
-            //Prints if player1 answered Stand
+            //Say if they answered No
             if (player1Play >= 2)
             {
                 player1Play = 2
                 println(player1 + " answered No\n")
             }
-            //Prints if player1 answer Hit
+            //Say if they answered Yes
             else 
             {
                 println(player1 + " answered Yes\n")
@@ -111,16 +127,25 @@ for (int i = 0; i < numGames; i++)
                 pointP1 += hand1[1+handNum1].rank
                 handNum1++
             }
-            //Asks player1 if they like to hit or stand
             println("- " + player2 + " would you like to get a new card? (1 = Yes, 2 = No)\n")
-            player2Play = scan.nextInt()
-            //Prints i2 player2 answer Stand
+            //Dealer Logic
+            if (pointP2 <= 15)
+                player2Play = 1
+            else if (pointP2 > 15)
+            {
+                player2Play = (int)(Math.random()*10)+1
+                if (player2Play > 2)
+                    player2Play = 2
+                else if (player2Play <= 2)
+                    player2Play = 1
+            }
+            //Say if Dealer asnwered No
             if (player2Play >= 2)
             {
                 player2Play = 2
                 println(player2 + " answered No\n")
             }
-            //Prints if player2 answer Hit
+            //Say if Dealer answered Yes
             else
             {
                 println(player2 + " answered Yes\n") 
@@ -150,7 +175,7 @@ for (int i = 0; i < numGames; i++)
     else if (gameWon == 0)
         println("Tie!")
     if (numGames > 1)
-        games++
+    games++
 }
 //If the amount of games that was declared is above 1, print out the majority score for the multiple matches
 if (numGames > 1)
@@ -173,130 +198,3 @@ println("\n------------------------ End ------------------------\n")
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*   def drawPile = []
-    // the pile that we match cards against and/or discard matching cards for each turn
-    def discardPile = []
-
-    def initialCard = deck.deal()
-    discardPile.add(initialCard)
-
-    // Put the rest of the deck down as the drawPile
-    drawPile = deck.deal(deck.size())
-
-    // Now we have two piles: the draw pile has 52-14 cards 
-    // and the discard pile shows one card facing up
-
-    int playerIndex = 0
-    // The "game" loop. Each draw is a turn in the UNO game.
-    // While any player has at least one card, we will keep playing
-    while (hand1.size() > 0 && hand2.size() > 0)
-    {
-        // Get the current hand and player
-        def currentHand = hands[playerIndex]
-        def currentPlayer = players[playerIndex]
-
-        // Get the next index
-        playerIndex++
-        playerIndex = playerIndex % numPlayers // example: 1 mod 2 is 1
-
-        // Print out the current player and hand...
-        println currentPlayer + " is playing with hand: " + currentHand
-
-        // Get the next card to compare
-        Card compareCard = discardPile[discardPile.size() - 1]
-
-        Card matchedCard = null
-        while (!matchedCard)
-        {
-            // Try to find a match for current player
-            // We find the first match and that is also the highest card that matches
-            // To do this, we sort the cards by "descending" rank. This is EASY in Groovy
-
-            // Sort by rank and then reverse so get descending rank
-            currentHand.collect().sort { it.rank } // TODO: There may be an issue here with aces for some reason
-            currentHand = currentHand.reverse(true)
-
-            for (Card h in currentHand)
-            {
-                // If the rank or suit matches, save the matched card and break
-                if(h.compareRank(compareCard) == 0 || h.suit == compareCard.suit)
-                {
-                    matchedCard = h
-                    currentHand.remove(h)
-                    discardPile.add(h)
-                    match = true
-                    break // exit the loop
-                }
-                println "    draw"
-            }
-
-            // Refill the draw pile with the discard pile's cards if necessary...
-            if (drawPile.isEmpty() && !discardPile.isEmpty())
-            {
-                Collections.shuffle(discardPile)
-                drawPile.addAll(discardPile)
-                discardPile.clear()
-                def newInitialCard = drawPile.remove(drawPile.size() - 1)
-                discardPile.add(newInitialCard)
-            }
-    
-            // If no match, get a card from the draw pile and try again
-            if(!matchedCard)
-            {
-                currentHand.add(drawPile.remove(drawPile.size() - 1))
-            }
-            else
-            {
-                println "    match found: " + matchedCard
-            }
-        }    
-    }
-
-    // Determine the winner!
-    def winner
-    def winnerCardCount = 0
-    def loserCardCount = 0
-    if (hand2.size() > hand1.size())
-    {
-        winner = player1
-        winnerCardCount = hand1.size()
-        loserCardCount = hand2.size()
-        player1GamesWon++
-    }
-    else
-    {
-        winner = player2
-        winnerCardCount = hand2.size()
-        loserCardCount = hand1.size()
-        player2GamesWon++
-    }
-
-    // Announce the winner of the game
-    println ""
-    println ">>>>> " + winner + " wins with " + winnerCardCount  + " vs " + loserCardCount + " cards"
- }
-
-println ""
-// Announce the winner of the "best-of" series
-println(player1 + ": " + player1GamesWon + " games, " + player2 + ": " + player2GamesWon + " games")
-*/
